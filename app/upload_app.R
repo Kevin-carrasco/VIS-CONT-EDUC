8bca513d35d9024e56d05be105569e545b8f4e8c
Kevin-carrasco <kevin.carrasco@ug.uchile.cl>
2023-07-26 14:38:11 -0400

    update general

--- app/upload_app.R
+++ app/upload_app.R
@@ -1,8 +1,7 @@
 #------------------------------------------------------------------------------#
-# Proyect title: "Observatorio de Cohesion Social" 
-# Author(s): Julio Iturra Sanhueza & Juan Carlos Castillo
-# website: https://www.linkedin.com/in/jciturras/
-# e-mail: julioiturrasanhueza@gmail.com
+# Author(s): Kevin Carrasco
+# website: https://www.linkedin.com/in/jkevin-carrasco-q/
+# e-mail: kevin.carrasco@ug.uchile.cl
 #------------------------------------------------------------------------------#
 
 #********************************
@@ -11,27 +10,20 @@
 
 #Este archivo sirve principalmente para generar el deploy de la app en el servidor de shiny.apps
 
-
 # install.packages('rsconnect')
 # install.packages('here')
 # install.packages("tictoc") #timming de funciones
 
-
+shinyApp(ui = ui, server = server)
 library(rsconnect)
-library(here)
-
+library(tictoc)
 
 # Realizar el login de la cuenta y el token de acceso  --------------------
 
-rsconnect::setAccountInfo(name='juitsa', #account
-                          token='4E3E38F216E2433D6290D64905056C90',
-                          secret='lVM2gyMtbp/0pMKlsQ77FiCbXFy/gXzlGUq60apt')
-
-rsconnect::terminateApp(appName = "ocs-coes")
-
+rsconnect::setAccountInfo(name='kevincarrasco',
+                          token='AA612B64FAA971BC64F40E2468846444',
+                          secret='cq32nDEOl9g+1IxIwQ7++PJAub91IMvUP73HYPNV')
 
 tictoc::tic() #inicio...
 rsconnect::deployApp(forceUpdate = TRUE) #Subir App al server de shinyapps
- 
 tictoc::toc() #término...
-
